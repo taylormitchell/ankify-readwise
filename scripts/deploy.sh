2f05f018a4890535b31170ab3410b2b93e9093fd
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-10-16 08:32:08 -0400

    Persist last_run.txt across deploys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,17 @@
+# save the content of last_run.txt, if it exists
+if [ -f last_run.txt ]; then
+    cp ./dist/last_run.txt ./last_run.txt.bak
+fi
+# remove the dist folder and create a new one
 rm -rf dist
 mkdir dist
 cp src/* dist/
 cp -r node_modules dist/ 
 cp .env.production dist/.env
-cp last_run.txt dist/last_run.txt
+# restore the content of last_run.txt, if it exists
+if [ -f ./last_run.txt.bak ]; then
+    cp ./last_run.txt.bak dist/last_run.txt
+    rm ./last_run.txt.bak
+else
+    cp last_run.txt dist/last_run.txt
+fi
